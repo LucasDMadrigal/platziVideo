1ac375fc571c91c7f360525181084d8266a87754
lucasdmadrigal <lucasmadrigalb@gmail.com>
2021-01-21 02:23:47 -0300

    finalizado practico de React JS

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../assets/styles/components/Footer.scss';
+const Footer = () => (
+	<footer className="Footer">
+		<a href="/">Terminos de uso</a>
+		<a href="/">Declaracion de privacidad</a>
+		<a href="/">Centro de ayuda</a>
+	</footer>
+);
+
+export default Footer;
--- webpack.config.js
+++ webpack.config.js
@@ -39,6 +39,17 @@ module.exports = {
                     'css-loader',
                     'sass-loader'
                 ]
+            },
+            {
+                test: /\.(png|gif|jpg)$/,
+                use: [
+                    {
+                        'loader': 'file-loader',
+                        options: {
+                            name: 'assets/[hash].[ext]'
+                        }
+                    }
+                ]
             }
         ]
     },
